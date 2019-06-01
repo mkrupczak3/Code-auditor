d6c46e8ee00662b51e30650cbea993da0555387d
mkrupczak3 <mkrupczak3@gatech.edu>
2019-06-01 16:54:00 -0400

    Update genaudit.sh

--- genaudit.sh
+++ genaudit.sh
@@ -1,1 +1,3 @@
 #!/usr/env/bin/bash
+find . -type f -not -path '*/\.*' -exec grep -Il '.' {} \; | xargs awk 'FNR==1{print "--------------------------------------------------------------------------------"; print FILENAME; print "--------------------------------------------------------------------------------"; print ""; }{ print; print "" }' > allcode.txt
+pandoc allcode.txt -o allcode.pdf
